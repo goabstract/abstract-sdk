2d1bf037d86d4c4ca29e7421a8bf349df3729004
Tom Moor <tom.moor@gmail.com>
2018-10-18 11:33:41 -0700

    Deployment

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -74,9 +74,8 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl("doc1.html", language)}>Example Link</Button>
-            <Button href={docUrl("doc2.html", language)}>Example Link 2</Button>
+            <Button href={docUrl("installation", language)}>Get Started</Button>
+            <Button href={siteConfig.repoUrl}>GitHub</Button>
           </PromoSection>
         </div>
       </SplashContainer>
