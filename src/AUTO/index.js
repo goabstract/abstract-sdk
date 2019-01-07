d4bdb87015c963009cfe0d2ef69f818931e395c0
Andrew McCloud <andrew@amccloud.com>
2019-01-07 14:24:41 -0800

    lint

--- src/AUTO/index.js
+++ src/AUTO/index.js
@@ -8,7 +8,8 @@ type Options = OptionsCLI & OptionsAPI;
 // $FlowFixMe
 class AbstractAUTO implements AbstractInterface {
   constructor(options: Options) {
-    const cli = typeof AbstractCLI === "function" ? new AbstractCLI(options) : {};
+    const cli =
+      typeof AbstractCLI === "function" ? new AbstractCLI(options) : {};
     const api = new AbstractAPI(options);
 
     return new Proxy(this, {
