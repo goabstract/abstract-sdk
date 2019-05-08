3b405b5105c4383f76482f58a42b2dfe8875c25a
bitpshr <mail@bitpshr.net>
2019-05-08 15:42:07 -0400

    bugfix: use lastChangedAtSha for latest LayerDescriptor resolution

--- src/endpoints/Descriptors.js
+++ src/endpoints/Descriptors.js
@@ -5,13 +5,32 @@ import Endpoint from "./Endpoint";
 export default class Descriptors extends Endpoint {
   async getLatestDescriptor<T: ObjectDescriptor>(descriptor: T): Promise<T> {
     if (descriptor.sha !== "latest") return descriptor;
+
     const [commit] = await this.client.commits.list((descriptor: any), {
       limit: 1
     });
-    return {
+
+    let latestDescriptor: any = {
       ...descriptor,
       commitSha: commit.sha,
       sha: commit.sha
     };
+
+    if (
+      latestDescriptor.projectId &&
+      latestDescriptor.branchId &&
+      latestDescriptor.sha &&
+      latestDescriptor.fileId &&
+      latestDescriptor.layerId
+    ) {
+      const layer = await this.client.layers.info(latestDescriptor);
+      latestDescriptor = {
+        ...descriptor,
+        commitSha: layer.lastChangedAtSha,
+        sha: layer.lastChangedAtSha
+      };
+    }
+
+    return latestDescriptor;
   }
 }
