3fcab00bc95c1ba2b0df28fb8af91a6fc11578b4
Paul Bouchon <mail@bitpshr.net>
2019-06-04 20:07:03 -0400

    feature: add pages endpoint to CLI transport (#137)

--- src/endpoints/Pages.test.js
+++ src/endpoints/Pages.test.js
@@ -1,5 +1,5 @@
 // @flow
-import { mockAPI, API_CLIENT } from "../testing";
+import { mockAPI, mockCLI, API_CLIENT, CLI_CLIENT } from "../testing";
 
 describe("#info", () => {
   test("api", async () => {
@@ -7,6 +7,21 @@ describe("#info", () => {
       pages: [{ id: "page-id" }]
     });
     const response = await API_CLIENT.pages.info({
+      branchId: "branch-id",
+      fileId: "file-id",
+      pageId: "page-id",
+      projectId: "project-id",
+      sha: "sha"
+    });
+
+    expect(response).toEqual({ id: "page-id" });
+  });
+
+  test("cli", async () => {
+    mockCLI(["files", "project-id", "sha"], {
+      pages: [{ id: "page-id" }]
+    });
+    const response = await CLI_CLIENT.pages.info({
       branchId: "branch-id",
       fileId: "file-id",
       pageId: "page-id",
@@ -24,6 +39,20 @@ describe("#list", () => {
       pages: [{ id: "page-id" }]
     });
     const response = await API_CLIENT.pages.list({
+      branchId: "branch-id",
+      fileId: "file-id",
+      projectId: "project-id",
+      sha: "sha"
+    });
+
+    expect(response).toEqual([{ id: "page-id" }]);
+  });
+
+  test("cli", async () => {
+    mockCLI(["files", "project-id", "sha"], {
+      pages: [{ id: "page-id" }]
+    });
+    const response = await CLI_CLIENT.pages.list({
       branchId: "branch-id",
       fileId: "file-id",
       projectId: "project-id",
