6828c6377dc0d41f40521924509c9d93969ec36a
bitpshr <mail@bitpshr.net>
2020-02-14 13:16:24 -0500

    feat: full project CRUD

--- src/endpoints/Projects.js
+++ src/endpoints/Projects.js
@@ -1,6 +1,7 @@
 // @flow
 import querystring from "query-string";
 import type {
+  NewProject,
   OrganizationDescriptor,
   Project,
   ProjectDescriptor,
@@ -10,7 +11,7 @@ import Endpoint from "../endpoints/Endpoint";
 import { wrap } from "../util/helpers";
 
 const headers = {
-  "Abstract-Api-Version": "13"
+  "Abstract-Api-Version": "22"
 };
 
 export default class Projects extends Endpoint {
@@ -56,6 +57,83 @@ export default class Projects extends Endpoint {
         }
 
         return wrap(response.data.projects, response);
+      },
+      requestOptions
+    });
+  }
+
+  create(
+    descriptor: OrganizationDescriptor,
+    project: NewProject,
+    requestOptions: RequestOptions = {}
+  ) {
+    project.about = (project: any).description;
+
+    return this.configureRequest<Promise<Project>>({
+      api: async () => {
+        const response = await this.apiRequest(`projects`, {
+          method: "POST",
+          body: project
+        });
+        return wrap(response);
+      },
+      requestOptions
+    });
+  }
+
+  update(
+    descriptor: ProjectDescriptor,
+    project: Project,
+    requestOptions: RequestOptions = {}
+  ) {
+    project.about = (project: any).description;
+
+    return this.configureRequest<Promise<Project>>({
+      api: async () => {
+        const response = await this.apiRequest(
+          `projects/${descriptor.projectId}`,
+          {
+            method: "PUT",
+            body: project
+          }
+        );
+        return wrap(response);
+      },
+      requestOptions
+    });
+  }
+
+  delete(descriptor: ProjectDescriptor, requestOptions: RequestOptions = {}) {
+    return this.configureRequest<Promise<void>>({
+      api: () => {
+        return this.apiRequest(`projects/${descriptor.projectId}`, {
+          method: "DELETE"
+        });
+      },
+      requestOptions
+    });
+  }
+
+  archive(descriptor: ProjectDescriptor, requestOptions: RequestOptions = {}) {
+    return this.configureRequest<Promise<Project>>({
+      api: () => {
+        return this.apiRequest(`projects/${descriptor.projectId}/archive`, {
+          method: "PUT"
+        });
+      },
+      requestOptions
+    });
+  }
+
+  unarchive(
+    descriptor: ProjectDescriptor,
+    requestOptions: RequestOptions = {}
+  ) {
+    return this.configureRequest<Promise<Project>>({
+      api: () => {
+        return this.apiRequest(`projects/${descriptor.projectId}/unarchive`, {
+          method: "PUT"
+        });
       },
       requestOptions
     });
