46dd489cdb4fa484da4728ebe12b34d55affa524
Andrew McCloud <andrew@abstract.com>
2020-04-27 11:51:16 -0700

    lint

--- src/errors.js
+++ src/errors.js
@@ -9,7 +9,7 @@ export class BaseError extends Error {
   constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
-    
+
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
