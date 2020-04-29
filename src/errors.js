70006de3a58f691a31e3a443c644dca3c65f5d84
Andrew McCloud <andrew@abstract.com>
2020-04-28 19:27:33 -0700

    lint

--- src/errors.js
+++ src/errors.js
@@ -10,7 +10,7 @@ export class BaseError extends Error {
     super(message);
     this.name = this.constructor.name;
 
-    if (Error.captureStackTrace) { 
+    if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor); // This is a Node API that helps stack trace readability
     }
   }
