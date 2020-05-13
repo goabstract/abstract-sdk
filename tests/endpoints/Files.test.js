910ab9d3e9d0d546e49e3628ed444f486beaedd8
Vladimir Berezovsky <berezovsky@ex.ua>
2020-05-13 12:05:50 +0300

    chore: add test

--- tests/endpoints/Files.test.js
+++ tests/endpoints/Files.test.js
@@ -98,6 +98,32 @@ describe("files", () => {
       ]);
     });
 
+    test("api - commit", async () => {
+      mockAPI(
+        "/projects/project-id/branches/branch-id/commits/commit-id/files",
+        {
+          files: [
+            {
+              id: "file-id"
+            }
+          ]
+        }
+      );
+
+      const response = await API_CLIENT.files.list({
+        branchId: "branch-id",
+        projectId: "project-id",
+        commitId: "commit-id",
+        sha: "sha"
+      });
+
+      expect(response).toEqual([
+        {
+          id: "file-id"
+        }
+      ]);
+    });
+
     test("cli", async () => {
       mockCLI(["files", "list", "--project-id=project-id", "--sha=sha"], {
         files: [
