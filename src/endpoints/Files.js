de44f46e17778e9bf9adb27a478ab38a585638d8
Vladimir Berezovsky <berezovsky@ex.ua>
2020-05-18 14:51:37 +0300

    Remove unnecessary string

--- src/endpoints/Files.js
+++ src/endpoints/Files.js
@@ -59,14 +59,14 @@ export default class Files extends Endpoint {
 
     return this.configureRequest<Promise<File[]>>("list", {
       api: async () => {
-      const response = latestDescriptor.commitId
-        ? await this.apiRequest(
-            `projects/${latestDescriptor.projectId}/branches/${latestDescriptor.branchId}/commits/${latestDescriptor.commitId}/files`
-          )
-        : await this.apiRequest(
-            `projects/${latestDescriptor.projectId}/branches/${latestDescriptor.branchId}/files``projects/${latestDescriptor.projectId}/branches/${latestDescriptor.branchId}/files`
-         );
- 
+        const response = latestDescriptor.commitId
+          ? await this.apiRequest(
+              `projects/${latestDescriptor.projectId}/branches/${latestDescriptor.branchId}/commits/${latestDescriptor.commitId}/files`
+            )
+          : await this.apiRequest(
+              `projects/${latestDescriptor.projectId}/branches/${latestDescriptor.branchId}/files`
+            );
+
         return wrap(response.files, response);
       },
 
